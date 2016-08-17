f86cf0fe078af27fea4ba83ccaebc820e8e2c792
Giancarlo <gfabrizi@gmail.com>
2016-08-17 14:20:33 +0200

    challenge04: server setup

--- src/app.js
+++ src/app.js
@@ -1,1 +1,10 @@
-console.log(exclaim(repeat, "howdy"));
+// node node_modules/http-server/bin/http-server
+
+
+var httpServer = require("http-server");
+var path = require("path");
+
+var pathToHtmlAndJsFiles = path.join(__dirname, "../");
+var server = httpServer.createServer({ root: pathToHtmlAndJsFiles });
+server.listen(8080);
+console.log("Server is running on http://127.0.0.1:8080/  or alternatevely http://localhost:8080/");
